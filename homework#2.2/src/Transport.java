4b7909a03a9715b225254c7dd054bf06388d4bcb
ARTEM05052004s <artem.myrzabekov.55@gmail.com>
2023-04-30 18:42:26 +0600

    Initial commit

---
+++ homework#2.2/src/Transport.java
@@ -0,0 +1,18 @@
+public class Transport implements Printable{
+    private double volume;
+
+    public Transport(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    @Override
+    public void print() {
+        System.out.println(" " + getClass().getSimpleName()+
+                "\n Обьем: " + getVolume());
+
+    }
+}
